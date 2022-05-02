6388a56fc41dbfd3b80ed351307ed2cb87419c66
xgiuseppevetrano <xgiuseppevetrano@gmail.com>
2022-05-02 19:26:06 +0200

    add js logic

--- js/script.js
+++ js/script.js
@@ -1,17 +1,17 @@
-// Consegna:
-// Scrivi un programma che stampi in console i numeri da 1 a 100.
-// MILESTONE 1
-// Per i multipli di 3 stampi “Fizz” al posto del numero e per i multipli di 5 stampi Buzz.
-// Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.
-// MILESTONE 2
-// Dato un container nel DOM, appendi un elemento html con il numero o la stringa corretta.
-// MILESTONE 3
-// Applica uno stile differente a seconda del valore dell'indice per i multipli di 3, per i multipli di 5 e per i valori che sono sia multipli di 3 che di 5.
-// Prima di partire a scrivere codice poniamoci qualche domanda:
-// Come faccio a sapere se un numero è divisibile per un altro? Abbiamo visto qualcosa di particolare che possiamo usare? Come creare nuovi elementi html e appenderli al container?
-// Consigli del giorno:
-// 1. scriviamo sempre prima dei commenti in italiano per capire cosa vogliamo fare
-// 2. proviamo ad immaginare le operazioni che vogliamo far svolgere al nostro programma così come lo faremmo "a mano"
+// 1. stampare in console i numeri da 1 a 100.
 
-// ------
+
+// 2. Se i è multiplo di 3 stampare "Fizz" al posto del numero.
+
+
+// 3. Se i è multiplo di 5 stampare "Buzz" al posto del numero.
+
+
+// 4. Se i è multiplo sia di 3 che di 5 stampare "FizzBuzz" al posto del numero.
+
+
+// 5. Creare all'interno del div.row dei div.col con all'interno il numero corrispondente.
+
+
+// 6. Applicare una classe differente a seconda del valore dell'indice per i multipli di 3, di 5 e di entrambi.
 
