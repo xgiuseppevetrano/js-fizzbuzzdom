dc6dd4e504540662e81e1511877fe726d34cb28c
xgiuseppevetrano <xgiuseppevetrano@gmail.com>
2022-05-02 20:00:21 +0200

    add sixth

--- js/script.js
+++ js/script.js
@@ -22,10 +22,19 @@ const row = document.querySelector(".row");
 
 for (let i = 1; i <= 100; i++) {
     const col = document.createElement("div");
-    col.classList.add("col");
-    col.innerText = i;
     row.append(col);
+    // 6. Applicare una classe differente a seconda del valore dell'indice per i multipli di 3, di 5 e di entrambi.
+    if (i % 3 === 0 && i % 5 === 0) {
+        col.classList.add("col", "col-fizzbuzz");
+        col.innerText = "FizzBuzz";
+    } else if (i % 3 === 0) {
+        col.classList.add("col", "col-fizz");
+        col.innerText = "Fizz";
+    } else if (i % 5 === 0) {
+        col.classList.add("col", "col-buzz");
+        col.innerText = "Buzz";
+    } else {
+        col.classList.add("col");
+        col.innerText = i;
+    }
 }
-
-// 6. Applicare una classe differente a seconda del valore dell'indice per i multipli di 3, di 5 e di entrambi.
-
